e32782d532478cfc2d572a36d1fa26319432d9f7
ralfsbremmers <ralfs.b@investing.com>
2025-01-10 00:18:23 +0200

    updated

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -41,7 +41,8 @@ Added scrollToElement method because sometimes when element is below screen sele
 
       boolean isElementInView = (boolean) Selenide.executeJavaScript(
               "var rect = arguments[0].getBoundingClientRect();" +
-                      "return (rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth));",
+                      "return (rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight ||" +
+                      " document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth));",
               element
       );
 
--- src/test/java/steps/PersonalizedOfferSteps.java
+++ src/test/java/steps/PersonalizedOfferSteps.java
@@ -44,6 +44,4 @@ public class PersonalizedOfferSteps {
         pages.personalizedOfferPage.clickContinueButton();
     }
 
-
-
 }
