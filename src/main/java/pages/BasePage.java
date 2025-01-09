91be319a5d46262f51b23cb13421d0a34ebab9c0
ralfsbremmers <ralfs.b@investing.com>
2025-01-09 21:30:37 +0200

    update basepage

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -32,25 +32,6 @@ public class BasePage {
   public void typeIn(SelenideElement element, String text) {
     element.shouldBe(Condition.visible, ofSeconds(10)).sendKeys(text);
   }
-
-//  public void clickOnElementWhichIsNotVisible(SelenideElement element) {
-//    // Ensure the element is visible, enabled, and in the viewport (if not, scroll to it)
-//    element.shouldBe(Condition.visible, Duration.ofSeconds(10))
-//            .shouldBe(Condition.enabled);
-//
-//    // Scroll to element if it's not in the viewport
-//    if ((boolean) Selenide.executeJavaScript(
-//            "var rect = arguments[0].getBoundingClientRect();" +
-//                    "return (rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth));",
-//            element) == false) {
-//      Selenide.executeJavaScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element);
-//      sleep(500);  // Wait for the scroll to finish
-//    }
-//
-//    // Click on the element
-//    element.click();
-//  }
-
 }
 
 
